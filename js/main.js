cbaea17d1f7579d687855829209d47e1c5d5ca94
Koblova <alina.kirov@mail.ru>
2022-01-21 22:23:14 +0300

    create: added newsletter

--- js/main.js
+++ js/main.js
@@ -13,3 +13,27 @@ var mySwiper = new Swiper(".swiper-container", {
     onlyInViewport: true,
   },
 });
+
+
+var myMap;
+
+ymaps.ready(init);
+
+function init () {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    myMap = new ymaps.Map('map', {
+        // При инициализации карты обязательно нужно указать
+        // её центр и коэффициент масштабирования.
+        center: [55.76, 37.64], // Москва
+        zoom: 10
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+
+    document.getElementById('destroyButton').onclick = function () {
+        // Для уничтожения используется метод destroy.
+        myMap.destroy();
+    };
+
+}
